0b6815988a137b5f0d7ef621d5e1c71252353047
Aaron Mendez <aarxnmendezz@gmail.com>
2025-01-07 00:32:33 +0100

    add dark logo and responsive design

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const themeToggle = document.getElementById('theme-toggle')
     // Obtener el icono del botón
     const themeIcon = themeToggle.querySelector('.material-icons-round')
+    // Obtener el logo
+    const logo = document.querySelector('.logo')
 
     // Evento de clic para cambiar el tema
     themeToggle.addEventListener('click', () => {
@@ -11,8 +13,19 @@ document.addEventListener('DOMContentLoaded', () => {
         // Cambiar el icono según el tema
         if (document.body.classList.contains('light-mode')) {
             themeIcon.textContent = 'dark_mode'
+            logo.src = 'images/logo_dark.png'
         } else {
             themeIcon.textContent = 'light_mode'
+            logo.src = 'images/logo.png'
         }
     })
+
+    // Comprobar el tema inicial al cargar la página
+    if (document.body.classList.contains('light-mode')) {
+        themeIcon.textContent = 'dark_mode'
+        logo.src = 'images/logo_dark.png'
+    } else {
+        themeIcon.textContent = 'light_mode'
+        logo.src = 'images/logo.png'
+    }
 })
