7f71eb9fca3dc9a4552c87b2064f3175a0b4e428
Aaron Mendez <aarxnmendezz@gmail.com>
2025-11-06 22:50:14 +0100

    refactor: organize component exports and update page imports
    - Add barrel export in components/index.ts for all sections
    - Update index.astro pages to import from centralized export
    - Simplify component imports using cleaner paths
    
    Refs #8

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export { default as HeroSection } from "./sections/HeroSection.astro";
+export { default as TechnologiesSection } from "./sections/TechnologiesSection.astro";
+export { default as ExperienceSection } from "./sections/ExperienceSection.astro";
+export { default as ProjectsSection } from "./sections/ProjectsSection.astro";
+export { default as EducationSection } from "./sections/EducationSection.astro";
+export { default as AboutSection } from "./sections/AboutSection.astro";
+export { default as ContactSection } from "./sections/ContactSection.astro";
